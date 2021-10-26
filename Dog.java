ca9e1f52e03c01bdd174eb98d86ac6e30d4cb2d0
polyqubit <polyumbrail@gmail.com>
2021-10-26 12:22:06 -0700

    Got summary to work

--- Dog.java
+++ Dog.java
@@ -1,6 +1,6 @@
 import java.util.Random;
 public class Dog {
-  private String name,breed,tA,tB;
+		private String name,breed,tA,tB,gender;
   private int age,data,genome;
   private boolean genomeInitialized = false;
   private Random rand = new Random();
@@ -45,7 +45,7 @@ public class Dog {
   public Dog(String name) {
     this.name = name;
     age = 0;
-    genome = 0x0000;
+    genome = 0x0100;
   }
   public Dog(String name, int age) {
     this.name = name;
@@ -55,32 +55,12 @@ public class Dog {
   public void setGenome() {
     genome = rand.nextInt(65536);
     genomeInitialized = true;
-	int a = nibbleExtract(genome, 1);
-	int b = nibbleExtract(genome, 2);
-	int c = nibbleExtract(genome, 3);
-	int d = nibbleExtract(genome, 4);
-    switch(a) {
-		case 0: breed = new String("German Shepherd");
-		case 1: breed = new String("Pomeranian");
-		case 2: breed = new String("Pug");
-		case 3: breed = new String("Golden Retriever");
-		case 4: breed = new String("Shiba Inu");
-		case 5: breed = new String("Poodle");
-		case 6: breed = new String("Dachshund");
-		case 7: breed = new String("Bulldog");
-		case 8: breed = new String("Dalmatian");
-		case 9: breed = new String("Tibetan Mastiff");
-		case 10: breed = new String("Corgi");
-		case 11: breed = new String("Beagle");
-		case 12: breed = new String("Collie");
-		case 13: breed = new String("Great Pyrenees");
-		case 14: breed = new String("Shetland Sheepdog");
-		case 15: breed = new String("Alaskan Mamalute");
-	}
+	updateGenome();
   }
   public void editGenome(int genome) {
     if(genomeInitialized){
       this.genome = genome;
+	  updateGenome();
     }
   }
   public void readGenome() {
@@ -88,27 +68,70 @@ public class Dog {
       System.out.println(String.format("0x%08X", genome));
     }
   }
+  public void updateGenome() {
+	int a = nibbleExtract(genome, 1);
+	int b = nibbleExtract(genome, 2);
+	int c = nibbleExtract(genome, 3);
+	int d = nibbleExtract(genome, 4);
+	System.out.println(a+", "+b+", "+c+", "+d);
+    switch(a) {
+		case 0: breed = new String("German Shepherd"); break;
+		case 1: breed = new String("Pomeranian"); break;
+		case 2: breed = new String("Pug"); break;
+		case 3: breed = new String("Golden Retriever"); break;
+		case 4: breed = new String("Shiba Inu"); break;
+		case 5: breed = new String("Poodle"); break;
+		case 6: breed = new String("Dachshund"); break;
+		case 7: breed = new String("Bulldog"); break;
+		case 8: breed = new String("Dalmatian"); break;
+		case 9: breed = new String("Tibetan Mastiff"); break;
+		case 10: breed = new String("Corgi"); break;
+		case 11: breed = new String("Beagle"); break;
+		case 12: breed = new String("Collie"); break;
+		case 13: breed = new String("Great Pyrenees"); break;
+		case 14: breed = new String("Shetland Sheepdog"); break;
+		case 15: breed = new String("Alaskan Malamute"); break;
+		default: breed = new String("Dog"); break;
+	}
+	switch(b) {
+		case 0: case 1: case 10: tA = new String("Competitive"); break;
+		case 2: case 3: case 11: tA = new String("Gluttonous"); break;
+		case 4: case 5: case 12: tA = new String("Stubborn"); break;
+		case 6: case 7: case 13: tA = new String("mrpoole"); break;
+		case 8: case 9: case 14: tA = new String("Smart"); break;
+		default: tA = new String("Competitive"); break;
+	}
+	switch(c) {
+		case 0: case 1: case 10: tB = new String("Friendly"); break;
+		case 2: case 3: case 11: tB = new String("Aggressive"); break;
+		case 4: case 5: case 12: tB = new String("Hyperactive"); break;
+		case 6: case 7: case 13: tB = new String("Muscular"); break;
+		case 8: case 9: case 14: tB = new String("Lazy"); break;
+		default: tA = new String("Friendly"); break;
+	}
+	switch(d) {
+		case 1: case 2: case 3: case 4: case 5: case 6: case 7: case 8: gender = new String("Male"); break;
+		default: gender = new String("Female"); break;
+	}
+  }
   public void rename(String name) {
     this.name = name;
   }
-  public int nibbleExtract(int inp, int digit) {
+  private int nibbleExtract(int inp, int digit) {
     int bit = inp >> (digit-1)*4;
     return bit & 0x000F;
     //return bit;
   }
   public void summary() {
-    //NOOO I DONT WANT TO DO 500 SWITCH STATEMENTS
-    //oh well
     if(genomeInitialized){
-      int b = nibbleExtract(genome, 1);
-      switch(breed) {case}
-      System.out.println("w");
+		System.out.println("Your dog, "+name+" is "+tA+", "+tB+" and "+gender+". ");
+		System.out.println("Your dog is "+age+" years old and is a "+breed);
     }
   }
 }
 
 /**
-0 - competititve, takes 0.5x as many repetitions to teach a trick and gains 1.5x more friendship from activities if another dog is present
+0 - competitive, takes 0.5x as many repetitions to teach a trick and gains 1.5x more friendship from activities if another dog is present
 1 - friendly, gains friendship 2x faster from all sources
 2 - glutton, hunger gain 2x faster
 3 - aggressive, gains friendship 0.5x as fast
