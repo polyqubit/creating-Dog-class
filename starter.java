4ad3d0b12c8175f06c7b09713251e84ad20e145f
polyqubit <89955098+polyqubit@users.noreply.github.com>
2021-10-26 15:14:15 -0700

    added PetCo

--- starter.java
+++ starter.java
@@ -1,10 +1,23 @@
-import pkg.*;
-import java.util.ArrayList;
+import java.util.Scanner;
 
 public class starter{
 	public static void main(String args[]) {
-		Dog dogtest = new Dog("broke",1);
-		dogtest.setGenome();
-		dogtest.summary();
-	}
+    Scanner sc = new Scanner(System.in);
+    System.out.println("Welcome to PetCo! Are you here to adopt an animal?\nType 1,2, or 3 for a dog, cat, or bird, or any other letter to quit.");
+    String inp = sc.nextLine();
+    if(inp.equals("1")||inp.equals("2")||inp.equals("3")) {
+      switch(inp) {
+        case "1":
+          System.out.println("Great! Follow me this way.");
+        break;
+        case "2":
+          System.out.println("Sorry, we don't have any of those at the moment...(i was lazy lol)");
+        break;
+        case "3":
+          System.out.println("Sorry, we don't have any of those at the moment...(i was lazy lol)");
+        break;
+        //the other animals will be included when i feel like it
+      }
+    }
+  }
 }
